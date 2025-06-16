6e998e3e10951571a27254d1793801cd0a269d5a
Kenny Tan <kennysjtan@gmail.com>
2025-06-15 23:17:59 -0700

    working on mobile navbar

--- pcc-webp/src/components/Navbar/Navbar.jsx
+++ pcc-webp/src/components/Navbar/Navbar.jsx
@@ -3,17 +3,21 @@
   Description: Sticky navigation bar with react-scroll navigation.
   Author: Kendrick Tan
 */
-import React from "react";
+import React, { useState } from "react";
 
-// Logo
+// Asset imports
 import pcc_logo from "../../assets/pcc_logo.png";
+import menu_icon from "../../assets/menu.png";
 
 const Navbar = () => {
   // Map Navbar items for convenience
   const navItems = ["HOME", "ABOUT", "PRODUCT", "ROOTS", "CONTACT"];
 
+  // useState to set mobile menu
+  const [mobileMenu, setMobileMenu] = useState(false);
+
   return (
-    <nav className="flex items-center justify-between px-6 py-4 bg-offwhite">
+    <nav className="relative flex items-center justify-between px-6 py-4 bg-offwhite">
       {/* ==== Logo ==== */}
       <img
         src={pcc_logo}
@@ -35,12 +39,26 @@ const Navbar = () => {
       </ul>
 
       {/* ==== Menu Icon (Mobile) ==== */}
-      <span
-        className="sm:invisible material-symbols-outlined text-charcoal cursor-pointer"
-        style={{ fontSize: "32px", lineHeight: "1" }}
-      >
-        menu
-      </span>
+      <img
+        src={menu_icon}
+        alt="Menu"
+        onClick={() => setMobileMenu(!mobileMenu)}
+        className="block sm:hidden cursor-pointer"
+      />
+
+      {mobileMenu && (
+        <ul className="absolute top-full right-0 w-[150px] h-[85vh] bg-offwhite shadow-lg p-6 flex flex-col gap-6 text-charcoal text-right font-sans sm:hidden z-50">
+          {navItems.map((item) => (
+            <li
+              key={item}
+              className="hover:text-accent transition-colors duration-200 cursor-pointer"
+              onClick={() => setMobileMenu(false)} // Close on click
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 };
