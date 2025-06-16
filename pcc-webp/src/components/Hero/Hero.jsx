58921c8fde031d6cd99edcf228e057d92ae4e063
Kenny Tan <kennysjtan@gmail.com>
2025-06-15 23:18:08 -0700

    init hero section

--- pcc-webp/src/components/Hero/Hero.jsx
+++ pcc-webp/src/components/Hero/Hero.jsx
@@ -8,8 +8,10 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section class="bg-primary text-offwhite">
-      <div class="text-center text-5xl font-bold font-sans">Hero Section</div>
+    <section className="bg-primary text-offwhite">
+      <div className="text-center text-5xl font-bold font-sans">
+        Hero Section
+      </div>
     </section>
   );
 };
